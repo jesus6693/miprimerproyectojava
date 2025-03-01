76824a7aa1b4603d7a05d3df2fab92e92055857f
jesus <jesustorres200503@gmail.com>
2025-03-01 07:25:28 -0500

    ejercicio I/O

---
+++ src/ejercicios.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ejercicios {
+    public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese su nombre: ");
+        String nombre = scanner.next();
+        System.out.print("Ingrese su edad: ");
+        byte edad = scanner.nextByte();
+
+        System.out.println("Hola, " + nombre + ". Tienes " + edad + " años.");
+        scanner.close();
+    }
+}
