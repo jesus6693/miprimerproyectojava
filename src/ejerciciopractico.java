43b39a87ee55f43b3dd935a8923999e93cf998fa
jesus <jesustorres200503@gmail.com>
2025-03-07 07:37:13 -0500

    se agrego ejercicio de operadores

---
+++ src/ejerciciopractico.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ejerciciopractico {
+    
+    public static void main(String[]args){
+        Scanner input =new Scanner (System.in);
+        System.out.println("ingrese el primer numero");
+        int num1 = input.nextInt();
+        System.out.println("ingrese el segundo numero");
+        int num2 =input.nextInt();
+        System.out.println("ingrese el tercer numero ");
+        int num3 = input.nextInt();
+        System.out.println("¿el primer numero es mayor que el segundo y menor el que el tercero?"+ (num1>num2 && num1<num3));
+        input.close();
+    }
+}
